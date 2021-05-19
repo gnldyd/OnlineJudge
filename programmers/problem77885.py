54150bfca036dfb92b4c3bf6492b5ae1f9c78dc4
Hwiyong Chang <enghc95@gmail.com>
2021-05-19 14:28:01 +0900

    Added code programmers - 월간 코드 챌린지 시즌2 - 2개 이하로 다른 비트

--- programmers/problem77885.py
+++ programmers/problem77885.py
@@ -1,13 +1,15 @@
 # 월간 코드 챌린지 시즌2 - 2개 이하로 다른 비트
 
+# one line function
+# def solution(numbers):
+#     return [n + 2 ** (len(bin(n)[2:]) - bin(n)[2:].rfind('0') - 2) if bin(n)[-1] == '1' else n + 1 for n in numbers]
+
 def solution(numbers):
     answer = []
     for number in numbers:
         binary = bin(number)
         if binary[-1] == '1':
-            binary = binary[2:]
-            point = len(binary) - binary.rfind('0') - 2 # rfind() is none = -1
-            answer.append(number + 2 ** point)
+            answer.append(number + 2 ** (len(binary[2:]) - binary[2:].rfind('0') - 2))
         else:
             answer.append(number + 1)
     return answer
